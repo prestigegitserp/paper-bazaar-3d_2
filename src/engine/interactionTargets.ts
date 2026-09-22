6c8116db30d6c0afc8643ff7dc7d4b8d61b19b2e
prestigegitserp <mr.soheil.mortazavi@gmail.com>
2026-09-22 23:19:02 +0330

    perf: add compact interaction target registry

---
+++ src/engine/interactionTargets.ts
@@ -0,0 +1,16 @@
+import type { Object3D } from 'three'
+
+const targets = new Set<Object3D>()
+
+export function registerInteractionTarget(object: Object3D) {
+  targets.add(object)
+  return () => targets.delete(object)
+}
+
+export function getInteractionTargets() {
+  return Array.from(targets)
+}
+
+export function clearInteractionTargets() {
+  targets.clear()
+}
